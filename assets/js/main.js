aecba640ae5b83eac556150d1034a64bd17da332
Rivaldo <rietcoins@inbox.lt>
2021-04-11 14:47:23 +0200

    uh

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,5 +57,35 @@ sr.reveal('.work__img',{interval: 200});
 sr.reveal('.contact__input',{interval: 200}); 
 
 
+// tabs
+
+var tabLinks = document.querySelectorAll(".tablinks");
+var tabContent = document.querySelectorAll(".tabcontent");
+
+
+tabLinks.forEach(function(el) {
+   el.addEventListener("click", openTabs);
+});
+
+
+function openTabs(el) {
+   var btnTarget = el.currentTarget;
+   var country = btnTarget.dataset.country;
+
+   tabContent.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   tabLinks.forEach(function(el) {
+      el.classList.remove("active");
+   });
+
+   document.querySelector("#" + country).classList.add("active");
+   
+   btnTarget.classList.add("active");
+}
+
+
+
 
 
